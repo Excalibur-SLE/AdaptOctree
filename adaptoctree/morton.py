a8fd99ea72a70517caedbb86984f210d5040f310
Srinath Kailasa <srinathkailasa@gmail.com>
2020-10-22 10:35:47 +0100

    Add morton encoding from sundar et al

--- adaptoctree/morton.py
+++ adaptoctree/morton.py
@@ -6,14 +6,15 @@ import numpy as np
 
 
 @numba.njit
-def interleave(x, y):
+def interleave(x, y, z):
     """
-    Interleave two 32-bit integers, bitwise.
+    Interleave three 32-bit integers, bitwise.
 
     Parameters:
     -----------
     x : np.int32
     y : np.int32
+    z : np.int32
 
     Returns:
     --------
@@ -25,8 +26,50 @@ def interleave(x, y):
     for i in range(32):
         xi = x & (1 << i)
         yi = y & (1 << i)
+        zi = z & (1 << i)
 
-        interleaved |= xi << i
+        interleaved |= xi << (i+2)
         interleaved |= yi << (i+1)
+        interleaved |= zi << i
 
     return interleaved
+
+@numba.njit
+def bit_length(x):
+
+    res = 0
+
+    while x > 0:
+        x = x >> 1
+        res += 1
+
+    return res
+
+@numba.njit
+def morton_encode(anchor):
+    """
+    Use a node's anchor to find it's Morton encoding.
+
+    Parameters:
+    -----------
+    anchor : np.array(shape=(4,), dtype=np.int32)
+
+    Returns:
+    --------
+    np.int64
+    """
+
+    # Interleave anchor bits
+    interleaved = interleave(anchor[0], anchor[1], anchor[2])
+
+    # Append level
+    interleaved = interleaved << bit_length(anchor[-1]) | anchor[-1]
+
+    return interleaved
+
+
+if __name__ == "__main__":
+    res = morton_encode([0, 0, 0, 2])
+
+    print(res)
+    print(bin(res))
