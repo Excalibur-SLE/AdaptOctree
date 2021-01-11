6a31a731342c1d0322c893c309363aa7fa335c33
Srinath Kailasa <srinathkailasa@gmail.com>
2021-01-11 14:11:48 +0000

    Add tree api to init

--- adaptoctree/__init__.py
+++ adaptoctree/__init__.py
@@ -3,3 +3,5 @@ import adaptoctree.__version as __version
 __version__ = __version.__version__
 __description__ = __version.__description__
 __title__ = __version.__title__
+
+from adaptoctree.tree import Tree
