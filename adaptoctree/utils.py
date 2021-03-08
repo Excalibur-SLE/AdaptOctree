97a2fe0c77ca3be0d218674fc6aa00d7809ba66d
Srinath Kailasa <srinathkailasa@gmail.com>
2021-03-08 12:50:57 +0000

    FEAT: Configurable digest size (#24)

--- adaptoctree/utils.py
+++ adaptoctree/utils.py
@@ -4,9 +4,10 @@ Utility functions
 import hashlib
 
 
-def deterministic_hash(string):
+def deterministic_hash(string, digest_size=10):
     """
     Compute a simple deterministic hash.
     """
-    h = hashlib.md5(string.encode())
+    h = hashlib.blake2b(digest_size=digest_size)
+    h.update(string.encode())
     return int(h.hexdigest(), 16)
